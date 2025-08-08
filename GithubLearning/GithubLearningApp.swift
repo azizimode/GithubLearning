df38f36a911a6105df199199a38f0722cadacfff
MOD <aziziran9@gmail.com>
2025-08-08 20:32:01 +0330

    Initial Commit

---
+++ GithubLearning/GithubLearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubLearningApp.swift
+//  GithubLearning
+//
+//  Created by Mohammad on 8/8/25.
+//
+
+import SwiftUI
+
+@main
+struct GithubLearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
