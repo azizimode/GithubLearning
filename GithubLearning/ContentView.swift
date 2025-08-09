a8ce322f421e3c777e482a84a3f64c62950f2841
MOD <aziziran9@gmail.com>
2025-08-09 11:57:09 +0330

    The Click Me Button Added

--- GithubLearning/ContentView.swift
+++ GithubLearning/ContentView.swift
@@ -14,6 +14,13 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
+            
+            Button("Click Me"){}
+                .cornerRadius(.infinity)
+                .padding()
+                .buttonStyle(.borderedProminent)
+                .controlSize(.large)
+                
         }
         .padding()
     }
